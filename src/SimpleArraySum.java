a84dcf17b70f51d499eaae2948054a690081f38d
gimeno <r.gimenocallejas@gmail.com>
2016-03-18 13:19:29 +0000

    Update SimpleArraySum.java
    prueba actualizacion

--- src/SimpleArraySum.java
+++ src/SimpleArraySum.java
@@ -11,7 +11,7 @@ public class SimpleArraySum {
         for(int arr_i=0; arr_i < n; arr_i++){
             arr[arr_i] = in.nextInt();
         }
-
+//Esto es una prueba
         int total = 0;
         for (int i = 0; i < n; i++) {
             total += arr[i];
